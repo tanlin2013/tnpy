6e16612b9734247d1816dc2007cbefe9c6916ce1
tanlin2013 <tanlin2013@gmail.com>
2017-01-03 17:50:42 +0800

    Create Operation.py

---
+++ Python/Operation.py
@@ -0,0 +1,27 @@
+"""
+This file contains the fundamental operation functions for Tensor Network Algorithms. 
+"""
+
+import numpy as np
+from scipy import sparse
+import Primme
+
+def initialize_MPS():
+    """
+    Randomly initialize the MPS.
+    
+    # Parameters:
+      
+    # Returns:
+      
+    """
+    return
+
+
+def initialize_EnvLs():
+
+    return
+    
+def initialize_EnvRs():
+    
+    return
