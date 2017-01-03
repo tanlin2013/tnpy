b8be7ae944df2fbf319e7b7980e3926ece5edf8a
tanlin2013 <tanlin2013@gmail.com>
2017-01-03 17:23:59 +0800

    Create Algorithm.py

---
+++ Python/Algorithm.py
@@ -0,0 +1,22 @@
+"""
+This file contains several algorithms which are based on the Matrix Product State (MPS) ansatz. The main operations are based on Python module Numpy, Scipy and PRIMME. 
+
+* Infinite Size Density Matrix Renormalization Group (iDMRG)
+* Infinte Time Evolution Bond Decimation (iTEBD)
+* Finite Size Density Matrix Renormalization Group (fDMRG)
+* Finte Time Evolution Bond Decimation (fTEBD)
+
+"""
+
+import numpy as np
+from scipy import sparse
+import Primme
+
+class iDMRG:
+
+class iTEBD:
+
+class fDMRG:
+
+class fTEBD:
+
