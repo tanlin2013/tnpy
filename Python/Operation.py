6c9582ce2d409eec46433327f27c47e53d37b161
tanlin2013 <tanlin2013@gmail.com>
2017-01-03 18:37:01 +0800

    Update Operation.py

--- Python/Operation.py
+++ Python/Operation.py
@@ -1,22 +1,44 @@
 """
-This file contains the fundamental operation functions for Tensor Network Algorithms. 
+This file contains the fundamental functions for Tensor Network operations. 
 """
 
 import numpy as np
 from scipy import sparse
 import Primme
 
-def initialize_MPS():
-    """
-    Randomly initialize the MPS.
+class operation:
     
-    # Parameters:
-      
-    # Returns:
-      
-    """
-    return
-
+    def __init__(self,d,chi):
+        """
+        # Parameters:
+            d: float
+                The physical bond dimension which is associated to the dimension of single-particle Hilbert space.
+            chi: float
+                The visual bond dimension to be keep after the Singular Value Decomposition (SVD).               
+        """
+        self.d=d
+        self.chi=chi
+        
+    def initialize_MPS(self,whichMPS,svm,size):
+        """
+        Randomly initialize the MPS.
+    
+        # Parameters:
+            whichMPS: string, {'i','f'} 
+                If whichMPS='i', an infinite MPS is initialized. Otherwise if whichMPS='f', a finite MPS is created.  
+            svm: bool
+                Specify whether the Singular Value Matrices are needed. If Ture, the Singular Value Matrices will be returned. 
+            size: int, optional
+                If size='f', the system size is needed. 
+        
+        # Returns: 
+            Gs: list of ndarray
+                
+            Ls: list of ndarray
+                
+        """
+    
+        return
 
 def initialize_EnvLs():
 
@@ -25,3 +47,4 @@ def initialize_EnvLs():
 def initialize_EnvRs():
     
     return
+
