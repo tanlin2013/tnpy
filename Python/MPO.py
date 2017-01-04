861854ff9bbd6a2257ca554f4a3d1751777f5238
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-04 17:25:14 +0800

    Create MPO.py

---
+++ Python/MPO.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+Sp=np.array([[0,1],[0,0]],dtype=float)
+Sm=np.array([[0,0],[1,0]],dtype=float)
+Sz=np.array([[1,0],[0,-1]],dtype=float)
+I2=np.identity(2,dtype=float)
+O2=np.zeros((2,2),dtype=float)
+
+def Thirring():
+    
+    return M
