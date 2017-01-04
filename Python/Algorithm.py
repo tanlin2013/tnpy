dfc2c647e4a9b6c142a17b0212dd494a39aeef88
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-04 17:38:45 +0800

    Update Algorithm.py

--- Python/Algorithm.py
+++ Python/Algorithm.py
@@ -9,14 +9,38 @@ This file contains several algorithms which are based on the Matrix Product Stat
 """
 
 import numpy as np
-from scipy import sparse
-import Primme
 
 class iDMRG:
+  def __init__(self,Gs,SVMs,s,chi,tolerence):
+    """
+    Define the global objects of iDMRG class.
+    """
+    self.Gs=Gs
+    self.SVMs=SVMs
+    self.d=d
+    self.chi=chi
+    self.tolerence=tolerence
+    
+  def effH(self):
+    
+  def update_MPS(self):  
 
+    
 class iTEBD:
-
+  def __init__(self):
+    
+  def time_evolution(self):   
+    
+    
+    
 class fDMRG:
-
+  def __init__(self):
+  
+  def effH(self):
+  
+  def variational_update(self):
+  
+  
+  
 class fTEBD:
-
+  def __init__(self):
