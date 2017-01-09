42d22b1f5dcd64d229df1572c6fc3a0bd0b20742
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-10 02:09:32 +0800

    Create example.py

---
+++ Python/example.py
@@ -0,0 +1,24 @@
+import numpy as np
+import Operation.MPS as MPS
+import Algorithm
+imoprt Measurement
+import MPO
+
+N=  # system size
+d=2  # physical bond dim
+D=   # visual bond dim of MPO   
+chi=  # visual bond dim of MPS
+
+if __name__=main:
+
+    MPS=MPS()
+    Gs,SVMs=MPS.initialize_MPS()
+    
+    elem=[]
+    M=MPO.MPO(elem)
+    
+    simulation=Algorithm.fDMRG()
+    =simulation.variational_optimize()
+    Gs=simulation.Gs()
+    
+    
