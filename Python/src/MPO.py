417b497bc055865f56a54cdc5b99f090730a497c
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-10 01:16:46 +0800

    Update MPO.py

--- Python/src/MPO.py
+++ Python/src/MPO.py
@@ -1,11 +1,32 @@
 import numpy as np
 
-Sp=np.array([[0,1],[0,0]],dtype=float)
-Sm=np.array([[0,0],[1,0]],dtype=float)
-Sz=np.array([[1,0],[0,-1]],dtype=float)
-I2=np.identity(2,dtype=float)
-O2=np.zeros((2,2),dtype=float)
+class MPO:
+    def __init__(self,whichMPS,N,D,elem):
+        self.whichMPS=whichMPS
+        self.N=N
+        self.D=D
+        self.elem=elem
+        self.Sp=np.array([[0,1],[0,0]],dtype=float)
+        self.Sm=np.array([[0,0],[1,0]],dtype=float)
+        self.Sz=0.5*np.array([[1,0],[0,-1]],dtype=float)
+        self.I2=np.identity(2,dtype=float)
+        self.O2=np.zeros((2,2),dtype=float)
 
-def MPO():
-    
-    return M
+    def MPO(self,site):
+        M=np.array(elem)
+        if whichMPS=='f':
+            if site==0:
+                M=M[0,:,:,:]                      
+                M=np.swapaxes(M,0,1)
+            elif site==self.N-1:
+                M=M[:,self.D-1,:,:]
+                M=np.swapaxes(M,0,1)        
+            else:               
+                M=np.swapaxes(M,0,3)
+                M=np.swapaxes(M,0,2)
+                M=np.swapaxes(M,1,3)
+        elif whichMPS=='i':            
+            M=np.swapaxes(M,0,3)
+            M=np.swapaxes(M,0,2)
+            M=np.swapaxes(M,1,3)
+        return M
