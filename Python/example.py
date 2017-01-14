058e6a908e8787f92d62711d0990286315470e32
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-14 22:56:21 +0800

    Update example.py

--- Python/example.py
+++ Python/example.py
@@ -1,24 +1,39 @@
 import numpy as np
-import Operation.MPS as MPS
+import sys ; sys.path.append("./src")
+import Operation
 import Algorithm
 import Measurement
 import MPO
 
-N=  # system size
-d=2  # physical bond dim
-D=   # visual bond dim of MPO   
-chi=  # visual bond dim of MPS
+if __name__=='__main__':
 
-if __name__=main:
+    whichMPS="f"
+    N=10  # system size
+    d=2  # physical bond dim
+    D=3   # visual bond dim of MPO   
+    chi=10  # visual bond dim of MPS
 
-    MPS=MPS()
-    Gs,SVMs=MPS.initialize_MPS()
+    MPS=Operation.MPS(whichMPS,d,chi)
+    Gs=MPS.initialize_MPS(N)
     
-    elem=[]
-    M=MPO.MPO(elem)
-    
-    simulation=Algorithm.fDMRG()
-    =simulation.variational_optimize()
+    MPO=MPO.MPO(whichMPS,N,D)
+    Sp=np.array([[0,1],[0,0]],dtype=float)
+    Sm=np.array([[0,0],[1,0]],dtype=float)
+    Sz=0.5*np.array([[1,0],[0,-1]],dtype=float)
+    I2=np.identity(2,dtype=float)
+    O2=np.zeros((2,2),dtype=float)
+
+    elem=[[Sp,Sm,I2],
+          [O2,O2,Sm],
+          [O2,O2,Sp]]
+    MPO.elem=elem    
+    M=MPO.MPO()
+
+    simulation=Algorithm.fDMRG(M,Gs,N,d,chi)
+    E,stats=simulation.variational_optimize()
     Gs=simulation.Gs()
+
+
+
     
     
