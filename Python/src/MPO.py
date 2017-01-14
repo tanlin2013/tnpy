ef7763b7b9cf693bae197c559f988b22ca977443
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-14 22:35:23 +0800

    Update MPO.py

--- Python/src/MPO.py
+++ Python/src/MPO.py
@@ -1,22 +1,17 @@
 import numpy as np
 
 class MPO:
-    def __init__(self,whichMPS,N,D,elem):
+    def __init__(self,whichMPS,N,D):
         self.whichMPS=whichMPS
         self.N=N
         self.D=D
-        self.elem=elem
-        self.Sp=np.array([[0,1],[0,0]],dtype=float)
-        self.Sm=np.array([[0,0],[1,0]],dtype=float)
-        self.Sz=0.5*np.array([[1,0],[0,-1]],dtype=float)
-        self.I2=np.identity(2,dtype=float)
-        self.O2=np.zeros((2,2),dtype=float)
-
+        self.elem=None
+        
     def MPO(self):
         MPOlist=[]
         for site in xrange(self.N):
-            M=np.array(self.elem(site))
-            if whichMPS=='f':
+            M=np.array(self.elem)
+            if self.whichMPS=='f':
                 if site==0:
                     M=M[0,:,:,:]                      
                     M=np.swapaxes(M,0,1)
@@ -27,7 +22,7 @@ class MPO:
                     M=np.swapaxes(M,0,3)
                     M=np.swapaxes(M,0,2)
                     M=np.swapaxes(M,1,3)
-            elif whichMPS=='i':            
+            elif self.whichMPS=='i':            
                 M=np.swapaxes(M,0,3)
                 M=np.swapaxes(M,0,2)
                 M=np.swapaxes(M,1,3)
