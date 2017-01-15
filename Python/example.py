787b191b27332a5e632f8a015a8827a18d022d96
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-15 20:21:08 +0800

    Update example.py

--- Python/example.py
+++ Python/example.py
@@ -10,7 +10,7 @@ if __name__=='__main__':
     whichMPS="f"
     N=10  # system size
     d=2  # physical bond dim
-    D=3   # visual bond dim of MPO   
+    D=5  # visual bond dim of MPO   
     chi=10  # visual bond dim of MPS
 
     MPS=operation.MPS(whichMPS,d,chi)
@@ -20,15 +20,17 @@ if __name__=='__main__':
         MPO=operators.MPO(whichMPS,N,D)
         Sp,Sm,Sz,I2,O2=operators.spin_operators()
 
-        elem=[[Sp,Sm,I2],
-              [O2,O2,Sm],
-              [O2,O2,Sp]]
+        elem=[[I2,Sp,Sm,Sz,O2],
+              [O2,O2,O2,O2,Sm],
+              [O2,O2,O2,O2,Sp],
+              [O2,O2,O2,O2,Sz],
+              [O2,O2,O2,O2,I2]]
         M=MPO.assign_to_MPO(elem,site)    
         return M
 
-    simulation=algorithm.fDMRG(M,Gs,N,d,chi)
+    simulation=algorithm.fDMRG(M,Gs,N,d,chi,tolerance=1e-6)
     E,stats=simulation.variational_optimize()
-    Gs=simulation.Gs()
+    Gs=simulation.Gs
 
 
 
