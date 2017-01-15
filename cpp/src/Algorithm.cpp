54d4bab5a25be81a232a1378a8f9d45ad3e8bb42
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-15 23:09:55 +0800

    Update and rename cpp/Algorithm.cpp to cpp/src/Algorithm.cpp

---
+++ cpp/src/Algorithm.cpp
@@ -0,0 +1,34 @@
+/******************************************************
+This file contains several algorithms which are based on the Matrix Product State (MPS) ansatz.
+
+* Infinite Size Density Matrix Renormalization Group (iDMRG)
+* Infinte Time Evolution Bond Decimation (iTEBD)
+* Finite Size Density Matrix Renormalization Group (fDMRG)
+* Finte Time Evolution Bond Decimation (fTEBD)
+
+******************************************************/
+
+#include <iostream>
+#include <stdexcept>
+#include <tuple>
+#include <uni10.hpp>
+#include <primme.h>
+using namespace std;
+using namespace uni10;
+
+class iDMRG{
+
+}
+
+class iTEBD{
+
+}
+
+
+class fDMRG{
+
+}
+
+class fTEBD{
+
+}
