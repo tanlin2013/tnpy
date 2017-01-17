c4e17c997a57b43a53f30392f586b77878c68060
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-17 16:53:44 +0800

    Update and rename cpp/src/operation.cpp to cpp/src/lib/operation.cpp

--- cpp/src/lib/operation.cpp
+++ cpp/src/lib/operation.cpp
@@ -2,35 +2,26 @@
 This file contains the fundamental operations for Tensor Network algorithm.
 **************************************************************************/
 
-#include <iostream>
-#include <stdexcept>
-#include <tuple>
-#include <uni10.hpp>
-#include <primme.h>
-using namespace std;
-using namespace uni10;
+tuple <double,double> MPS::initialize_iMPS(){
+    vector<UniTensor> Gs;
+    vector<Matrix> SVMs;
+    vsctor<bonds> bonds;
+    
+    for (int site=0; site<2; site++){
+            Gs.push_back(UniTensor G(bonds).randomize());
+            SVMs.push_back(Matrix SVM(chi,chi).randomize());
+        }
+    return make_tuple(Gs,SVMs);
+}
 
-class MPS{
-    public:
-        string whichMPS;
-        int d;
-        int chi;
-};
-
-tuple <double,double> MPS::initialize_MPS(string canonical_form=NULL, int N=NULL){
+double MPS::initialize_fMPS(string canonical_form=NULL, int N=NULL){
     vector<UniTensor> Gs;
     vector<Matrix> SVMs;
     vector<Bond> bonds;
     bonds.push_back(Bond bdi(BD_IN,chi));
     bonds.push_back(Bond bdo1(BD_OUT,d));
     bonds.push_back(Bond bdo2(BD_OUT,chi));
-    if (whichMPS=='i'){
-        for (int site=0; site<2; site++){
-            Gs.push_back(UniTensor G(bonds).randomize());
-            SVMs.push_back(Matrix SVM(chi,chi).randomize());
-        }
-    }
-    else if (whichMPS=='f'){
+    
         for (int site=0; site<N; site++){
             if (site==0){
                 Gs.push_back(UniTensor G().randomize());
@@ -43,27 +34,17 @@ tuple <double,double> MPS::initialize_MPS(string canonical_form=NULL, int N=NULL
             }
             SVMs.push_back(Matrix SVM(chi,chi).randomize());
         }
-    }
-    else{
-        throw invalid_argument("MPS must be either iMPS or fMPS.");
-    }
+   
     return make_tuple(Gs,SVMs);
 }
 
-double MPS::initialize_EnvLs(){
-
-
+void normalize_fMPS(){
 
 }
 
-double MPS::initialize_EnvRs(){
-
-
+void to_GL_rep(){
 
 }
-
-
-
 double eigensolver(){
 
 }
