62561500463bc61096030fa57d30b2882d60824f
Tan Tao-Lin <tanlin2013@gmail.com>
2017-02-15 01:53:20 +0800

    Update Thirring.py

--- Python/Thirring.py
+++ Python/Thirring.py
@@ -9,8 +9,9 @@ import measurement
 import operators
 
 class Thirring:
-    def __init__(self,N,g,m,mu,lamda,S_target):
+    def __init__(self,N,a,g,m,mu,lamda,S_target):
         self.N=N
+        self.a=a
         self.g=g
         self.m=m
         self.mu=mu
@@ -21,11 +22,11 @@ class Thirring:
         MPO=operators.MPO(whichMPS='f',N=self.N,D=6)
         Sp,Sm,Sz,I2,O2=operators.spin_operators()
         
-        beta=-0.5*self.J*(self.delta+eta*self.deltap)+0.5*((-1.0)**site*self.m+self.mu)-self.lamda*self.S_target
-        gamma=self.lamda*(0.25+self.S_target**2/self.l)-0.25*self.J*(self.delta+eta*self.deltap)+0.5*self.mu
+        beta=0.5*self.g+0.5*self.a*((-1.0)**site*self.m+self.mu)-self.lamda*self.S_target
+        gamma=self.lamda*(0.25+self.S_target**2/self.N)+0.25*self.g+0.5*self.a*self.mu
         parity=(site+1)%2
               
-        elem=[[I2,-0.5*self.J*Sp, -0.5*self.J*Sm, 0.5*np.sqrt(self.lamda)*Sz, -0.5*self.J*(parity*self.delta+0.5*self.deltap)*Sz, gamma*I2+beta*Sz],
+        elem=[[I2,-0.5*Sp, -0.5*Sm, 0.5*np.sqrt(self.lamda)*Sz, 0.5*self.g*parity*Sz, gamma*I2+beta*Sz],
                     [O2,O2,O2,O2,O2,Sm],
                     [O2,O2,O2,O2,O2,Sp],
                     [O2,O2,O2,I2,O2,np.sqrt(self.lamda)*Sz],
@@ -39,15 +40,16 @@ if __name__=='__main__':
 
     N=10  # system size  
     chi=10  # visual bond dim of MPS
-    g=0.5 
-    m=0.1
-    mu=0.0
-    lamda=0.0
+    a=1.0 # lattice spacing
+    g=0.5 # coupling constant
+    m=0.1 # bare mass
+    mu=0.0 # chemical potential
+    lamda=0.0 # penalty strength
     S_target=0.0
 
     MPS=TN.MPS(whichMPS='f',d=2,chi)
     Gs=MPS.initialize_MPS(N)
-    model=Thirring(N,g,m,mu,lamda,S_target)
+    model=Thirring(N,a,g,m,mu,lamda,S_target)
     
     simulation=algorithm.fDMRG(model.M,Gs,N,d,chi,tolerance=1e-6)
     E,stats=simulation.variational_optimize()
