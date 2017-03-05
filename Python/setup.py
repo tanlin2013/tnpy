672c0177aae5dab9efe50b45da64555d38636fe1
Tan Tao-Lin <tanlin2013@gmail.com>
2017-03-05 22:56:23 +0800

    Update setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
 
+from distutils.core import setup
+
+setup(
+    name = 'tnsa',
+    packages = ['src', 'data_analysis'],
+    description = 'Tensor Network State Algorithms',
+    author = 'tanlin'
+)
