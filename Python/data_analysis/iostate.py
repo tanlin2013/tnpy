47af1fabe152b2b48ab97615059515b9b3b88ae9
Tan Tao-Lin <tanlin2013@gmail.com>
2017-03-05 17:39:39 +0800

    Update iostate.py

--- Python/data_analysis/iostate.py
+++ Python/data_analysis/iostate.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 
 def save(path,state):
@@ -9,3 +10,14 @@ def read(path):
     state=[npzfile['arr_%i'%i] for i in xrange(len(npzfile.files))]
     npzfile.close()
     return state    
+
+def grep(filename,pattern):
+    found=False ; line_idx=[]
+    for idx,line in enumerate(open(filename)):
+        if pattern in line:   
+            found=True ; line_idx.append(idx)
+    if not found:
+        print "pattern '{}' does not be found in file {}".format(pattern,filename)
+        return None
+    else:            
+        return line_idx
