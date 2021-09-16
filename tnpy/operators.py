913f1bfd8e080bbd92e8bf2373c69ad5e2fde868
taolin <tanlin2013@gmail.com>
2021-09-17 00:25:32 +0800

    Fix: spin of ladder operators

--- tnpy/operators.py
+++ tnpy/operators.py
@@ -12,8 +12,8 @@ class SpinOperators:
     def __new__(cls, spin: float = 0.5):
         super(SpinOperators, cls).__init__(spin)
         SOp = namedtuple('SpinOperators', ['Sp', 'Sm', 'Sz', 'I2', 'O2'])
-        return SOp(Sp=np.array([[0, 1], [0, 0]], dtype=float),
-                   Sm=np.array([[0, 0], [1, 0]], dtype=float),
+        return SOp(Sp=spin * np.array([[0, 2], [0, 0]], dtype=float),
+                   Sm=spin * np.array([[0, 0], [2, 0]], dtype=float),
                    Sz=spin * np.array([[1, 0], [0, -1]], dtype=float),
                    I2=np.identity(2, dtype=float),
                    O2=np.zeros((2, 2), dtype=float))
