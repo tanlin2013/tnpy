e6a58395d7eec76b479341e77f49925ebbb67ac6
taolin <tanlin2013@gmail.com>
2021-10-29 11:22:21 +0800

    Doc: update mathjax path for html rendering

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ intersphinx_mapping = {
 }
 
 # Mathjax
-mathjax_path = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
