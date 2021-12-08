cf3b789d46fa2992393df2cfb3ba00ea1b0da4c4
taolin <tanlin2013@gmail.com>
2021-12-08 13:28:58 +0800

    Feature: add entropy in ed

--- tnpy/exact_diagonalization.py
+++ tnpy/exact_diagonalization.py
@@ -19,3 +19,12 @@ class ExactDiagonalization(FullHamiltonian):
 
     def _eigen_solver(self) -> Tuple[np.ndarray, np.ndarray]:
         return np.linalg.eigh(self.matrix)
+
+    def reduced_density_matrix(self, site: int, energy_level: int = 0) -> np.ndarray:
+        to_shape = (self.physical_dimensions ** (site + 1), -1) if site < self.N // 2 \
+            else (-1, self.physical_dimensions ** (self.N - site))
+        return np.square(np.linalg.svd(self.evecs[:, energy_level].reshape(to_shape), compute_uv=False))
+
+    def entanglement_entropy(self, site: int, energy_level: int = 0) -> float:
+        rho = self.reduced_density_matrix(site, energy_level)
+        return -1 * rho @ np.log(rho)
