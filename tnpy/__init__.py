1bfb3c363d3286b14716896fb30829bb60a260c2
taolin <tanlin2013@gmail.com>
2022-01-26 00:43:20 +0800

    Publish: release version 0.0.6

--- tnpy/__init__.py
+++ tnpy/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 logging.basicConfig(format='%(asctime)s [%(filename)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logging.root.setLevel(level=logging.INFO)
