c994b96379a7891e39179bebe4641889faeb9d13
taolin <tanlin2013@gmail.com>
2022-03-16 18:09:50 +0800

    Refactor: separate convergence handler

--- tnpy/finite_dmrg.py
+++ tnpy/finite_dmrg.py
@@ -1,8 +1,7 @@
 import time
 import numpy as np
 from datetime import timedelta
-from itertools import count
-from tqdm import tqdm
+from itertools import cycle
 from tnpy import logger
 from tnpy.linalg import eigh, eigshmv
 from tnpy.matrix_product_state import MatrixProductState, Environment, Direction
@@ -69,6 +68,7 @@ class FiniteDMRG:
 
     def sweep(self, direction: Direction = Direction.rightward, tol: float = 1e-8, **kwargs) -> float:
         """
+        Perform a single sweep on the given direction.
 
         Args:
             direction:
@@ -89,6 +89,19 @@ class FiniteDMRG:
             self._env.update(site, direction=direction)
         return energy
 
+    def _converged(self, energy_gradient: float, tol: float, n_sweep: int, max_sweep: int) -> bool:
+        if abs(energy_gradient) < tol:
+            logger.info(f"Reaching set tolerance {tol}, stop sweeping.")
+            return True
+        elif n_sweep == max_sweep:
+            # @TODO: dump mps to file and raise error
+            logger.warning(f"Maximum number of sweeps {max_sweep} is reached, "
+                           f"yet dE/N = {energy_gradient:e} is greater than tol = {tol}.")
+        elif abs(energy_gradient) > tol and energy_gradient < 0:
+            logger.warning(f"Fail on lowering energy in this sweep, "
+                           f"got dE/N = {energy_gradient:e}, skip and proceed.")
+        return False
+
     def run(self, tol: float = 1e-7, max_sweep: int = 100, **kwargs) -> List[float]:
         """
 
@@ -100,28 +113,21 @@ class FiniteDMRG:
         Returns:
 
         """
-        clock, energies = [time.process_time()], []
+        clock, energies = [time.process_time()], [np.nan]
         logger.info(f"Set tolerance = {tol}, up to maximally {max_sweep} sweeps.")
-        for n_sweep in count(start=1):
-            logger.info(f"In sweep epoch [{n_sweep}/{max_sweep}]")
-            rightward_energy = self.sweep(Direction.rightward, tol=tol, **kwargs)
-            leftward_energy = self.sweep(Direction.leftward, tol=tol, **kwargs)
-            energy_gradient = (leftward_energy - rightward_energy) / self.n_sites
-            energies.extend([rightward_energy, leftward_energy])
+        for n_sweep, direction in zip(range(1, max_sweep + 1), cycle([Direction.rightward, Direction.leftward])):
+            logger.info(f"<<==== In sweep epoch [{n_sweep}/{max_sweep}] ====>>")
+            energy = self.sweep(direction, tol=tol, **kwargs)
+            energy_gradient = (energies[-1] - energy) / self.n_sites
             clock.append(time.process_time())
-            if abs(energy_gradient) < tol:
-                logger.info(f"Reaching set tolerance {tol}.")
+            energies.append(energy)
+            logger.info(f"This sweep takes {timedelta(seconds=np.diff(clock[-2:])[0])} "
+                        f"and lowers dE/N by {energy_gradient:e}.")
+            if self._converged(energy_gradient, tol, n_sweep, max_sweep):
                 break
-            elif n_sweep == max_sweep:
-                # @TODO: dump mps to file and raise error
-                logger.warning(f"Maximum number of sweeps {max_sweep} reached, "
-                               f"yet dE/N = {energy_gradient} > tol = {tol}.")
-                break
-            elif abs(energy_gradient) > tol and energy_gradient < 0:
-                logger.warning(f"Fail on lowering energy in this sweep, "
-                               f"got dE/N = {energy_gradient}, skip and proceed.")
         elapsed_time = np.mean(np.sort(np.diff(clock))[:3])
-        logger.info(f"{n_sweep} sweeps, best of 3 - {timedelta(seconds=elapsed_time)} per sweep.")
+        logger.info(f"Summary - {n_sweep} sweeps, "
+                    f"best of {min(3, n_sweep)} - {timedelta(seconds=elapsed_time)} per sweep.")
         return energies
 
 
