7963f5a133a3cc67e6e4d85a3ea9b2fc5c4932fd
taolin <tanlin2013@gmail.com>
2022-04-06 19:21:02 +0800

    Feature: add module config

--- tnpy/__init__.py
+++ tnpy/__init__.py
@@ -1,8 +1,21 @@
 import logging
+import yaml
+from pathlib import Path
 
-__version__ = '0.0.9'
+
+# -- Load configurations from yaml file ----------------------
+class ConfigReader:
+    def __init__(self, file):
+        self.__dict__.update(**yaml.safe_load(file))
 
 
+with open(Path(__file__).absolute().parent / 'config.yaml', 'r') as f:
+    config = ConfigReader(f)
+
+# -- Version ----------------------
+__version__ = config.version
+
+# -- Define logger and the associated formatter and handler ----------------------
 formatter = logging.Formatter('%(asctime)s [%(filename)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 handler = logging.StreamHandler()
