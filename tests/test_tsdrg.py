8ae7954a3687a1ba4a4ca320c6cc024d67924f43
taolin <tanlin2013@gmail.com>
2022-04-24 23:53:57 +0800

    Test: refactor with pytest

--- tests/test_tsdrg.py
+++ tests/test_tsdrg.py
@@ -1,6 +1,3 @@
-import imghdr
-from itertools import product
-
 import pytest
 import numpy as np
 
@@ -9,33 +6,37 @@ from tnpy.exact_diagonalization import ExactDiagonalization
 from tnpy.tsdrg import (
     TensorTree,
     TreeTensorNetworkSDRG as tSDRG,
-    TreeTensorNetworkMeasurements
 )
 
 
 class TestTensorTree:
-
-    @pytest.fixture(scope='class')
+    @pytest.fixture(scope="class")
     def tree(self):
         model = RandomHeisenberg(n=4, h=0, penalty=0, s_target=0)
         tree = TensorTree(model.mpo)
         tree.fuse(
-            left_id=1, right_id=2, new_id=4,
-            data=ExactDiagonalization(
-                RandomHeisenberg(n=2, h=0).mpo
-            ).evecs.reshape((2, 2, 4))
+            left_id=1,
+            right_id=2,
+            new_id=4,
+            data=ExactDiagonalization(RandomHeisenberg(n=2, h=0).mpo).evecs.reshape(
+                (2, 2, 4)
+            ),
         )
         tree.fuse(
-            left_id=0, right_id=4, new_id=5,
-            data=ExactDiagonalization(
-                RandomHeisenberg(n=3, h=0).mpo
-            ).evecs.reshape((2, 4, 8))
+            left_id=0,
+            right_id=4,
+            new_id=5,
+            data=ExactDiagonalization(RandomHeisenberg(n=3, h=0).mpo).evecs.reshape(
+                (2, 4, 8)
+            ),
         )
         tree.fuse(
-            left_id=5, right_id=3, new_id=6,
-            data=ExactDiagonalization(
-                RandomHeisenberg(n=4, h=0).mpo
-            ).evecs.reshape((8, 2, 16))
+            left_id=5,
+            right_id=3,
+            new_id=6,
+            data=ExactDiagonalization(RandomHeisenberg(n=4, h=0).mpo).evecs.reshape(
+                (8, 2, 16)
+            ),
         )
         return tree
 
@@ -58,9 +59,7 @@ class TestTensorTree:
 
     def test_fuse(self, tree):
         with pytest.raises(RuntimeError):
-            tree.fuse(
-                left_id=6, right_id=5, new_id=7, data=np.random.rand(16, 16, 16)
-            )
+            tree.fuse(left_id=6, right_id=5, new_id=7, data=np.random.rand(16, 16, 16))
 
     def test_find_path(self, tree):
         assert tree.find_path(0) == [6, 5]
@@ -106,16 +105,15 @@ class TestTensorTree:
 
 
 class TestTreeTensorNetworkSDRG:
-
-    @pytest.fixture(scope='class')
+    @pytest.fixture(scope="class")
     def model(self):
-        return RandomHeisenberg(n=10, h=0.0, penalty=0, s_target=0)
+        return RandomHeisenberg(n=6, h=0.0, penalty=0, s_target=0)
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture(scope="class")
     def tsdrg(self, model):
         return tSDRG(model.mpo, chi=2 ** 6)
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture(scope="class")
     def ed(self, model):
         return ExactDiagonalization(model.mpo)
 
@@ -123,100 +121,104 @@ class TestTreeTensorNetworkSDRG:
     def test_block_hamiltonian(self, tsdrg, site):
         np.testing.assert_array_equal(
             np.array(
-                [[0.25, 0, 0, 0],
-                 [0, -0.25, 0.5, 0],
-                 [0, 0.5, -0.25, 0],
-                 [0, 0, 0, 0.25]]
+                [
+                    [0.25, 0, 0, 0],
+                    [0, -0.25, 0.5, 0],
+                    [0, 0.5, -0.25, 0],
+                    [0, 0, 0, 0.25],
+                ]
             ),
-            tsdrg.block_hamiltonian(site)
+            tsdrg.block_hamiltonian(site),
         )
 
     def test_spectrum_projector(self, tsdrg):
         evecs = np.array(
-            [[0, 1, 0, 0],
-             [1 / np.sqrt(2), 0, 1 / np.sqrt(2), 0],
-             [-1 / np.sqrt(2), 0, 1 / np.sqrt(2), 0],
-             [0, 0, 0, 1]]
+            [
+                [0, 1, 0, 0],
+                [1 / np.sqrt(2), 0, 1 / np.sqrt(2), 0],
+                [-1 / np.sqrt(2), 0, 1 / np.sqrt(2), 0],
+                [0, 0, 0, 1],
+            ]
         )
         projector = tsdrg.spectrum_projector(locus=3, evecs=evecs)
-        np.testing.assert_array_equal(
-            evecs.reshape((2, 2, 4)),
-            projector
+        np.testing.assert_array_equal(evecs.reshape((2, 2, 4)), projector)
+
+    def test_run(self, ed, tsdrg):
+        tsdrg.run()
+        np.testing.assert_allclose(
+            ed.evals[: tsdrg.chi], np.sort(tsdrg.evals), atol=1e-12
         )
 
-    def test_run(self):
-        import sys
-        np.set_printoptions(threshold=sys.maxsize)
-        self.tsdrg.run()
-        evals = self.tsdrg.evals
-        idx = np.argsort(evals)
-        # print(self.ed.evals[np.where(np.logical_and(self.ed.evals >= min(evals), self.ed.evals <= max(evals)))])
-        print(evals[idx])
-        var = self.tsdrg.measurements.expectation_value(self.tsdrg.mpo.square()) \
-            - np.square(self.tsdrg.measurements.expectation_value(self.tsdrg.mpo))
-        print(var[idx])
-        self.tsdrg.tree.plot().render(format='png', view=True)
-        # np.testing.assert_allclose(
-        #     self.ed.evals[:self.tsdrg.chi],
-        #     # self.tsdrg.measurements.expectation_value(self.tsdrg.mpo),
-        #     evals[idx],
-        #     atol=1e-12
-        # )
+    def test_measurements(self, ed, tsdrg):
+        tsdrg.run()
+        np.testing.assert_allclose(
+            ed.evals[: tsdrg.chi],
+            tsdrg.measurements.expectation_value(tsdrg.mpo),
+            atol=1e-12,
+        )
 
 
 class TestTreeTensorNetworkMeasurements:
+    @pytest.fixture(scope="class")
+    def model(self):
+        return RandomHeisenberg(n=8, h=0.5, penalty=0, s_target=0)
 
-    def __init__(self, *args, **kwargs):
-        super(TestTreeTensorNetworkMeasurements, self).__init__(*args, **kwargs)
-        self.model = RandomHeisenberg(n=8, h=0.5, penalty=0, s_target=0)
-        self.chi = 2 ** 8
-        self.tsdrg = tSDRG(self.model.mpo, self.chi)
-        self.tsdrg.run()
-        self.measurer = TreeTensorNetworkMeasurements(self.tsdrg.tree)
-        self.ed = ExactDiagonalization(self.model.mpo)
+    @pytest.fixture(scope="class")
+    def tsdrg(self, model):
+        tsdrg = tSDRG(model.mpo, chi=2 ** 8)
+        tsdrg.run()
+        return tsdrg
 
-    def test_sandwich(self):
+    @pytest.fixture(scope="class")
+    def ed(self, model):
+        return ExactDiagonalization(model.mpo)
+
+    def test_sandwich(self, tsdrg):
         np.testing.assert_allclose(
-            self.tsdrg.evals,
-            np.diag(self.measurer.sandwich(self.tsdrg.mpo).data),
-            atol=1e-12
+            tsdrg.evals,
+            np.diag(tsdrg.measurements.sandwich(tsdrg.mpo).data),
+            atol=1e-12,
         )
         np.testing.assert_allclose(
-            np.identity(self.chi),
-            self.measurer.sandwich().data,
-            atol=1e-12
+            np.identity(tsdrg.chi), tsdrg.measurements.sandwich().data, atol=1e-12
         )
 
-    def test_expectation_value(self):
-        spectral_folding_tsdrg = tSDRG(self.model.mpo.square(), self.chi)
+    def test_expectation_value(self, model, ed):
+        spectral_folding_tsdrg = tSDRG(model.mpo.square(), chi=2 ** 8)
         spectral_folding_tsdrg.run()
         np.testing.assert_allclose(
-            self.ed.evals,
-            np.sort(spectral_folding_tsdrg.measurements.expectation_value(self.model.mpo)),
-            atol=1e-12
+            ed.evals,
+            np.sort(spectral_folding_tsdrg.measurements.expectation_value(model.mpo)),
+            atol=1e-12,
         )
 
-    def test_min_surface(self):
-        for site in range(self.tsdrg.n_sites - 1):
-            min_side, surface, _ = self.measurer._min_surface(site)
-            # print(site, surface)
+    @pytest.mark.parametrize("site", list(range(8 - 1)))
+    def test_min_surface(self, site, tsdrg):
+        min_side, surface, _ = tsdrg.measurements._min_surface(site)
+        # print(site, surface)
 
-    def test_reduced_density_matrix(self):
+    @pytest.mark.parametrize("site", list(range(8 - 1)))
+    @pytest.mark.parametrize("level_idx", list(range(2 ** 8))[:1])
+    def test_reduced_density_matrix(self, site, level_idx, ed, tsdrg):
         # TODO: With the absence of disorder, h = 0, these tests fail for level_idx > 0
         # self.tsdrg.tree.plot().render(format='png', view=True)
-        for site, level_idx in product(range(self.tsdrg.n_sites - 1), range(self.chi)[:1]):
-            # print(site, level_idx)
-            np.testing.assert_allclose(
-                self.ed.reduced_density_matrix(site=site, level_idx=level_idx),
-                np.linalg.eigvalsh(self.measurer.reduced_density_matrix(site=site, level_idx=level_idx))[::-1],
-                atol=1e-12
-            )
+        np.testing.assert_allclose(
+            ed.reduced_density_matrix(site=site, level_idx=level_idx),
+            np.linalg.eigvalsh(
+                tsdrg.measurements.reduced_density_matrix(
+                    site=site, level_idx=level_idx
+                )
+            )[::-1],
+            atol=1e-12,
+        )
 
-    def test_entanglement_entropy(self):
-        for site, level_idx in product(range(self.tsdrg.n_sites - 1), range(self.chi)[:1]):
-            # print(site, level_idx)
-            self.assertAlmostEqual(
-                self.ed.entanglement_entropy(site=site, level_idx=level_idx, nan_to_num=True),
-                self.measurer.entanglement_entropy(site=site, level_idx=level_idx, nan_to_num=True),
-                places=12
-            )
+    @pytest.mark.parametrize("site", list(range(8 - 1)))
+    @pytest.mark.parametrize("level_idx", list(range(2 ** 8))[:1])
+    def test_entanglement_entropy(self, site, level_idx, ed, tsdrg):
+        np.testing.assert_allclose(
+            ed.entanglement_entropy(site=site, level_idx=level_idx, nan_to_num=True),
+            tsdrg.measurements.entanglement_entropy(
+                site=site, level_idx=level_idx, nan_to_num=True
+            ),
+            atol=1e-12,
+        )
