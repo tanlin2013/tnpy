f8aec7131452787af8ac36939a8fd91aeaf507f7
taolin <tanlin2013@gmail.com>
2022-04-27 16:38:30 +0800

    Fix: read version from package metadata

--- tnpy/__init__.py
+++ tnpy/__init__.py
@@ -1,7 +1,25 @@
 import logging
+from importlib import metadata
 from pathlib import Path
 
 import toml  # tomllib is coming out on python 3.11 as standard lib
+
+
+# -- Version ----------------------
+__version__ = metadata.version(__name__)
+
+# -- Define logger and the associated formatter and handler -------------
+formatter = logging.Formatter(
+    "%(asctime)s [%(filename)s] %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("tnpy")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 # -- Load configurations from toml file ----------------------
@@ -10,25 +28,5 @@ class ConfigReader:
         self.__dict__.update(**toml.load(file))
 
 
-with open(Path(__file__).absolute().parent / 'config.toml',
-          'r', encoding='utf-8') as f:
+with open(Path(__file__).absolute().parent / "config.toml", "r", encoding="utf-8") as f:
     config = ConfigReader(f)
-
-# -- Version ----------------------
-with open(Path(__file__).absolute().parents[1] / 'pyproject.toml',
-          'r', encoding='utf-8') as f:
-    __version__ = toml.load(f)["tool"]["poetry"]["version"]
-
-# -- Define logger and the associated formatter and handler -------------
-formatter = logging.Formatter(
-    '%(asctime)s [%(filename)s] %(levelname)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
-
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-handler.setFormatter(formatter)
-
-logger = logging.getLogger('tnpy')
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
