332f89de0bf767a304e57babb4105e3963e19b36
taolin <tanlin2013@gmail.com>
2022-06-14 22:24:59 +0800

    Style: reformat by black

--- tnpy/linalg.py
+++ tnpy/linalg.py
@@ -1,8 +1,9 @@
+from typing import Tuple, Union
+
 import primme
 import numpy as np
 import scipy.linalg as spla
 from scipy.sparse.linalg import LinearOperator
-from typing import Tuple, Union
 
 
 def svd(matrix: np.ndarray, cutoff: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -32,18 +33,20 @@ def qr(matrix: np.ndarray, cutoff: int) -> Tuple[np.ndarray, np.ndarray]:
     Returns:
 
     """
-    q, r = np.linalg.qr(matrix, mode='reduced')
+    q, r = np.linalg.qr(matrix, mode="reduced")
     q = q[:, 0:cutoff]
     r = r[0:cutoff, :]
     return q, r
 
 
-def eigh(matrix: np.ndarray, k: int = 1,
-         backend: str = 'numpy', **kwargs) -> Tuple[Union[float, np.ndarray], np.ndarray]:
+def eigh(
+    matrix: np.ndarray, k: int = 1, backend: str = "numpy", **kwargs
+) -> Tuple[Union[float, np.ndarray], np.ndarray]:
     """
 
     Args:
-        matrix: Hermitian or real symmetric matrices whose eigenvalues and eigenvectors are to be computed.
+        matrix: Hermitian or real symmetric matrices whose eigenvalues and eigenvectors
+            are to be computed.
         k: The number of eigenpairs to be computed.
         backend: ('numpy' or 'scipy').
         **kwargs: Keyword arguments for scipy solver.
@@ -52,15 +55,20 @@ def eigh(matrix: np.ndarray, k: int = 1,
 
     """
     evals, evecs = {
-        'numpy': np.linalg.eigh(matrix),
-        'scipy': spla.eigh(matrix, **kwargs)
+        "numpy": np.linalg.eigh(matrix),
+        "scipy": spla.eigh(matrix, **kwargs),
     }[backend]
     return (evals[0], evecs[:, 0]) if k == 1 else (evals[:k], evecs[:, :k])
 
 
-def eigshmv(linear_operator: LinearOperator, v0: np.ndarray,
-            k: int = 1, which: str = 'SA', tol: float = 0,
-            **kwargs) -> Tuple[Union[float, np.ndarray], np.ndarray]:
+def eigshmv(
+    linear_operator: LinearOperator,
+    v0: np.ndarray,
+    k: int = 1,
+    which: str = "SA",
+    tol: float = 0,
+    **kwargs
+) -> Tuple[Union[float, np.ndarray], np.ndarray]:
     """
 
     Args:
@@ -68,11 +76,14 @@ def eigshmv(linear_operator: LinearOperator, v0: np.ndarray,
         v0: Initial guesses to the eigenvectors.
         k: The number of eigenpairs to be computed.
         which: Which k eigenvectors and eigenvalues to find.
-        tol: Tolerance for eigenpairs (stopping criterion). The default value is sqrt of machine precision.
+        tol: Tolerance for eigenpairs (stopping criterion).
+            The default value is sqrt of machine precision.
         **kwargs: Keyword arguments for primme solver.
 
     Returns:
 
     """
-    evals, evecs = primme.eigsh(linear_operator, v0=v0, k=k, which=which, tol=tol, **kwargs)
+    evals, evecs = primme.eigsh(
+        linear_operator, v0=v0, k=k, which=which, tol=tol, **kwargs
+    )
     return (evals[0], evecs) if k == 1 else (evals, evecs)
