9d29d7058258ed45c33096f92ae72110be129d20
taolin <tanlin2013@gmail.com>
2022-06-21 22:52:53 +0800

    Refactor: rewrite with pytest

--- tests/test_exact_diagonalization.py
+++ tests/test_exact_diagonalization.py
@@ -1,97 +1,115 @@
-from unittest import TestCase
+import pytest
 import numpy as np
 import scipy.linalg as spla
+
 from tnpy.model import RandomHeisenberg
 from tnpy.exact_diagonalization import ExactDiagonalization
 from tnpy.operators import FullHamiltonian
 
 
-class TestExactDiagonalization(TestCase):
+class TestExactDiagonalization:
+    @pytest.fixture(scope="class")
+    def ed(self):
+        return ExactDiagonalization(RandomHeisenberg(n=2, h=0).mpo)
 
-    def __init__(self, *args, **kwargs):
-        super(TestExactDiagonalization, self).__init__(*args, **kwargs)
-        self.ed = ExactDiagonalization(RandomHeisenberg(n=2, h=0).mpo)
-
-    def test_matrix(self):
+    def test_matrix(self, ed):
         np.testing.assert_array_equal(
             np.array(
-                [[0.25, 0, 0, 0],
-                 [0, -0.25, 0.5, 0],
-                 [0, 0.5, -0.25, 0],
-                 [0, 0, 0, 0.25]]
+                [
+                    [0.25, 0, 0, 0],
+                    [0, -0.25, 0.5, 0],
+                    [0, 0.5, -0.25, 0],
+                    [0, 0, 0, 0.25],
+                ]
             ),
-            self.ed.matrix
+            ed.matrix,
         )
 
-    def test_evals(self):
-        np.testing.assert_array_equal(
-            np.array([-0.75, 0.25, 0.25, 0.25]),
-            self.ed.evals
-        )
+    def test_evals(self, ed):
+        np.testing.assert_array_equal(np.array([-0.75, 0.25, 0.25, 0.25]), ed.evals)
 
-    def test_evecs(self):
+    def test_evecs(self, ed):
         np.testing.assert_array_equal(
             np.array(
-                [[0, 1, 0, 0],
-                 [1/np.sqrt(2), 0, 1/np.sqrt(2), 0],
-                 [-1/np.sqrt(2), 0, 1/np.sqrt(2), 0],
-                 [0, 0, 0, 1]]
+                [
+                    [0, 1, 0, 0],
+                    [1 / np.sqrt(2), 0, 1 / np.sqrt(2), 0],
+                    [-1 / np.sqrt(2), 0, 1 / np.sqrt(2), 0],
+                    [0, 0, 0, 1],
+                ]
             ),
-            self.ed.evecs
+            ed.evecs,
         )
 
 
-class TestShiftInvertExactDiagonalization(TestCase):
+class TestShiftInvertExactDiagonalization:
+    @pytest.fixture(scope="class")
+    def model(self):
+        return RandomHeisenberg(n=8, h=0.5, penalty=0, s_target=0, seed=2021)
 
-    def __init__(self, *args, **kwargs):
-        super(TestShiftInvertExactDiagonalization, self).__init__(*args, **kwargs)
-        model = RandomHeisenberg(n=8, h=0.5, penalty=0, s_target=0, seed=2021)
-        self.ham = FullHamiltonian(model.mpo).matrix
-        self.evals, self.evecs = np.linalg.eigh(self.ham)
+    @pytest.fixture(scope="class")
+    def ham(self, model):
+        return FullHamiltonian(model.mpo).matrix
 
-        self.offset = 0.2
-        shifted_model = RandomHeisenberg(
-            n=model.n, h=model.h,
-            penalty=model.penalty, s_target=model.s_target,
-            seed=model.seed, offset=self.offset
+    @pytest.fixture(scope="class")
+    def eigen_solver(self, ham):
+        return np.linalg.eigh(ham)
+
+    @pytest.fixture(scope="class")
+    def offset(self):
+        return 0.1
+
+    @pytest.fixture(scope="class")
+    def shift_inverted_model(self, model, offset):
+        return RandomHeisenberg(
+            n=model.n,
+            h=model.h,
+            penalty=model.penalty,
+            s_target=model.s_target,
+            seed=model.seed,
+            offset=offset,
         )
-        self.shifted_ham = FullHamiltonian(shifted_model.mpo).matrix
-        self.sf_evals, sf_evecs = spla.eigh(
-            self.shifted_ham,
-            b=self.shifted_ham @ self.shifted_ham
-        )
-        self.restored_evals = 1 / self.sf_evals + self.offset
-        self.restored_evecs = self.shifted_ham @ sf_evecs
 
-    def test_restored_evals(self):
+    @pytest.fixture(scope="class")
+    def shift_inverted_ham(self, shift_inverted_model):
+        return FullHamiltonian(shift_inverted_model.mpo).matrix
+
+    @pytest.fixture(scope="class")
+    def shift_invert_eigen_solver(self, shift_inverted_ham, offset):
+        sf_inv_evals, sf_inv_evecs = spla.eigh(
+            shift_inverted_ham, b=shift_inverted_ham @ shift_inverted_ham
+        )
+        restored_evals = 1 / sf_inv_evals + offset
+        restored_evecs = shift_inverted_ham @ sf_inv_evecs
+        return restored_evals, restored_evecs
+
+    def test_restored_evals(self, ham, eigen_solver, shift_invert_eigen_solver):
+        evals, _ = eigen_solver
+        restored_evals, restored_evecs = shift_invert_eigen_solver
         np.testing.assert_allclose(
-            self.restored_evals,
-            np.diag(self.restored_evecs.T @ self.ham @ self.restored_evecs),
-            atol=1e-12
+            restored_evals,
+            np.diag(restored_evecs.T @ ham @ restored_evecs),
+            atol=1e-12,
         )
-        np.testing.assert_allclose(
-            np.sort(self.restored_evals),
-            self.evals,
-            atol=1e-12
-        )
+        np.testing.assert_allclose(np.sort(restored_evals), evals, atol=1e-12)
 
-    def test_restored_evecs(self):
-        idx = np.argsort(self.restored_evals)
-        np.testing.assert_allclose(
-            self.restored_evecs[:, idx],
-            self.evecs,
-            atol=1e-12
-        )
+    def test_restored_evecs(self, eigen_solver, shift_invert_eigen_solver):
+        _, evecs = eigen_solver
+        restored_evals, restored_evecs = shift_invert_eigen_solver
+        idx = np.argsort(restored_evals)
+        np.testing.assert_allclose(restored_evecs[:, idx], evecs, atol=1e-12)
+        # TODO: this will fail, but why?
 
-    def test_residual(self):
-        res = np.array([
-            self.ham @ self.restored_evecs[:, i] - self.restored_evals[i] * self.restored_evecs[:, i]
-            for i in range(len(self.restored_evals))
-        ])
+    def test_residual(self, ham, shift_invert_eigen_solver):
+        restored_evals, restored_evecs = shift_invert_eigen_solver
+        res = np.array(
+            [
+                ham @ restored_evecs[:, i] - restored_evals[i] * restored_evecs[:, i]
+                for i in range(len(restored_evals))
+            ]
+        )
         np.testing.assert_allclose(
-            np.linalg.norm(res, axis=0),
-            np.zeros(len(self.restored_evals)),
-            atol=1e-12
+            np.linalg.norm(res, axis=0), np.zeros(len(restored_evals)), atol=1e-12
         )
 
     # def test_visualize(self):
