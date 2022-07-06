5598440dc5211f825e7b3713f729c3addca5d4cf
taolin <tanlin2013@gmail.com>
2022-07-06 22:30:48 +0800

    Feature: add ed one and two-point functions

--- tnpy/exact_diagonalization.py
+++ tnpy/exact_diagonalization.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tnpy.operators import MatrixProductOperator, FullHamiltonian
-from typing import Tuple
+from typing import Tuple, Sequence
 
 
 class ExactDiagonalization(FullHamiltonian):
@@ -43,3 +43,63 @@ class ExactDiagonalization(FullHamiltonian):
         rho = self.reduced_density_matrix(site, level_idx)
         entropy = -1 * rho @ np.log(rho)
         return np.nan_to_num(entropy) if nan_to_num else entropy
+
+    @staticmethod
+    def kron_opts(operators: Sequence[np.ndarray]) -> np.ndarray:
+        opt = operators[0]
+        for next_opt in operators[1:]:
+            opt = np.kron(opt, next_opt)
+        return opt
+
+    def one_point_function(
+        self, operator: np.ndarray, site: int, level_idx: int = 0
+    ) -> np.ndarray:
+        assert operator.shape == (self.phys_dim, self.phys_dim)
+        opt_mat = self.kron_opts(
+            [
+                np.eye(self.phys_dim**site),
+                operator,
+                np.eye(self.phys_dim ** (self.n_sites - site - 1)),
+            ]
+        )
+        return self.evecs[:, level_idx].T @ opt_mat @ self.evecs[:, level_idx]
+
+    def two_point_function(
+        self,
+        operator1: np.ndarray,
+        operator2: np.ndarray,
+        site1: int,
+        site2: int,
+        level_idx: int,
+    ) -> np.ndarray:
+        assert operator1.shape == (self.phys_dim, self.phys_dim)
+        assert operator2.shape == (self.phys_dim, self.phys_dim)
+        assert site1 != site2
+        site1, site2 = sorted((site1, site2))
+        # TODO: operators aren't sorted accordingly
+        opt_mat = self.kron_opts(
+            [
+                np.eye(self.phys_dim**site1),
+                operator1,
+                np.eye(self.phys_dim ** (abs(site1 - site2) - 1)),
+                operator2,
+                np.eye(self.phys_dim ** (self.n_sites - site2 - 1)),
+            ]
+        )
+        return self.evecs[:, level_idx].T @ opt_mat @ self.evecs[:, level_idx]
+
+    def connected_two_point_function(
+        self,
+        operator1: np.ndarray,
+        operator2: np.ndarray,
+        site1: int,
+        site2: int,
+        level_idx: int,
+    ):
+        return self.two_point_function(
+            operator1, operator2, site1, site2, level_idx
+        ) - self.one_point_function(
+            operator1, site1, level_idx
+        ) * self.one_point_function(
+            operator2, site2, level_idx
+        )
