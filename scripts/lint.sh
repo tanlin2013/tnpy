0e00dc00a9b7471f14d6654bec0d92d5b67156f9
taolin <tanlin2013@gmail.com>
2023-02-01 14:44:33 +0800

    ci: add dev scripts

---
+++ scripts/lint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+poetry run cruft check
+poetry run safety check -i 39462 -i 40291
+poetry run bandit -r tnpy/
+poetry run isort --check --diff tnpy/ tests/
+poetry run black --check tnpy/ tests/
+poetry run flake8 tnpy/ tests/
+poetry run mypy \
+           --install-types \
+           --non-interactive \
+           tnpy/
+#  https://mypy.readthedocs.io/en/stable/running_mypy.html#library-stubs-not-installed
