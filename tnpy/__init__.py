6a4a543a81a69323a7ba2e7b765086cbd765720c
taolin <tanlin2013@gmail.com>
2023-03-02 16:23:48 +0800

    refactor: rm config through toml file

--- tnpy/__init__.py
+++ tnpy/__init__.py
@@ -1,8 +1,5 @@
 import logging
 from importlib import metadata
-from pathlib import Path
-
-import toml  # tomllib is coming out on python 3.11 as standard lib
 
 # -- Version -----------------------------------------------------------------
 
@@ -21,13 +18,3 @@ handler.setFormatter(formatter)
 logger = logging.getLogger("tnpy")
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
-
-
-# -- Load configurations from toml file --------------------------------------
-class ConfigReader:
-    def __init__(self, file):
-        self.__dict__.update(**toml.load(file))
-
-
-with open(Path(__file__).absolute().parent / "config.toml", "r", encoding="utf-8") as f:
-    config = ConfigReader(f)
